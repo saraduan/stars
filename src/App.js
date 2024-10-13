e6ebf8c74f8d062d2362de3c1b711b81efe9687e
Sara Duan <saraduan@Saras-Laptop.local>
2024-10-12 20:27:58 -0700

    redo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import "./stars.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <div className="logo">matcha</div>
+          <ul>
+            <li><a href="#projects">projects</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#team">team</a></li>
+            <li><a href="#contact" className="contact-button">contact us</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <section className="hero">
+        <div className="left">
+          <h1>Make something great</h1>
+          <button className="cta-button">Let's go</button>
+        </div>
+        <div className="right">
+          <img src="https://via.placeholder.com/300" alt="Man in motion" />
+        </div>
+      </section>
+
+      <section className="info">
+        <div className="info-box">
+          <p>This example is inspired by the work of Halo Lab, a design agency based in Ukraine.</p>
+          <small>dribbble.com/halolab</small>
+        </div>
+        <div className="info-box">
+          <p>We help businesses to make their brands shine on all platforms.</p>
+          <small>MEET OUR DESIGNERS</small>
+        </div>
+        <div className="info-box">
+          <img src="https://via.placeholder.com/100" alt="Color gradient example" />
+        </div>
+      </section>
     </div>
   );
 }
